ebc87034d09ab7394190df5abff7db62082646b4
Luis <jalvarezbretana@danielcastelao.org>
2022-12-14 13:35:39 +0100

    Actualización plugin con BD

--- wordpress/wp-content/plugins/malsonantes/malsonantes.php
+++ wordpress/wp-content/plugins/malsonantes/malsonantes.php
@@ -1,26 +1,98 @@
 <?php
 /*
 Plugin Name: Malsonantes
-Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is just a filter plugin.
 Author: Hadesito
-Version: 1.7.2
-Author URI: http://ma.tt/
+Version: 1.1
 */
-function caca( $text ) {
-    return str_replace( 'caca', 'popo', $text );
+function dB()
+{
+
+    global $wpdb;
+
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $malsonantes = $wpdb->prefix . 'malsonantes';
+
+    $tableCreate = "CREATE TABLE IF NOT EXISTS $malsonantes (
+    id mediumint(9) NOT NULL,
+    correctWord text NOT NULL,
+    PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    $sqlDlt = "DELETE FROM $malsonantes";
+    $wpdb->query($sqlDlt);
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sqlDlt);
+
+    $sqlIns = "INSERT INTO $malsonantes (id, correctWord)
+    VALUES (1, 'popo'),
+           (2,'trasero'),
+           (3,'feucho'),
+           (4,'nepe'),
+           (5,'flatulencia');";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($tableCreate);
+    dbDelta($sqlIns);
+
 }
-function culo( $text ) {
-    return str_replace( 'culo', 'pompis', $text );
-}function muerte( $text ) {
-    return str_replace( 'muerte', 'defunción', $text );
-}function pedo( $text ) {
-    return str_replace( 'pedo', 'flatulencia', $text );
-}function subnormal( $text ) {
-    return str_replace( 'subnormal', 'guapo', $text );
+
+add_action('plugins_loaded', 'dB');
+
+function substitute_words($text)
+{
+
+    $rudeWords = array("caca", "culo", "feo", "polla", "pedo");
+
+    global $wpdb;
+
+    $malsonantes = $wpdb->prefix . 'malsonantes';
+
+    $result = $wpdb->get_results("SELECT correctWord FROM " . $malsonantes, ARRAY_A);
+
+    foreach ($result as $row) {
+        $correctWords[] = $row['correctWord'];
+    }
+
+    return str_replace($rudeWords, $correctWords, $text);
 }
-add_filter( 'the_content', 'caca');
-add_filter( 'the_content', 'culo');
-add_filter( 'the_content', 'muerte');
-add_filter( 'the_content', 'pedo');
-add_filter( 'the_content', 'subnormal');
+
+
+add_action('plugins_loaded', 'substitute_words');
+add_filter('the_content', 'substitute_words');
+
+/**
+ * Añade un tabla a la BD
+ * @return void
+ */
+function myplugin_update_db_check()
+{
+    // Objeto global del WordPress para trabajar con la BD
+    global $wpdb;
+
+    // recojemos el
+    $charset_collate = $wpdb->get_charset_collate();
+
+    // le añado el prefijo a la tabla
+    $table_name = $wpdb->prefix . 'dam';
+
+    // creamos la sentencia sql
+    $sql = "CREATE TABLE $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+        name tinytext NOT NULL,
+        text text NOT NULL,
+        url varchar(55) DEFAULT '' NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    // libreria que necesito para usar la funcion dbDelta
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
+
+/**
+ * Ejecuta 'myplugin_update_db_check', cuando el plugin se carga
+ */
+add_action('plugins_loaded', 'myplugin_update_db_check');
